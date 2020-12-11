8449bfd530c606ca9c45d3e845f265ef3c2c200a
akhil10996 <akhil10996@gmail.com>
2020-12-11 13:55:18 +0530

    log

--- Sudoku.py
+++ Sudoku.py
@@ -29,37 +29,3 @@ grid =[
         [0, 4, 9, 2, 0, 6, 0, 0, 7] 
 
 ]
-
-=======
-#importing libraries
-import pygame
-
-# initialising font
-pygame.font.init()
-
-#Window
-screen= pygame.display.set_mode((600,600))
-
-#Title 
-pygame.display.set.caption("Sudoku Backtracking Solver")
-
-x= 0
-y=0
-dif = 600/9
-val=0
-
-#default sudoku board
-grid =[ 
-        [7, 8, 0, 4, 0, 0, 1, 2, 0], 
-        [6, 0, 0, 0, 7, 5, 0, 0, 9], 
-        [0, 0, 0, 6, 0, 1, 0, 7, 8], 
-        [0, 0, 7, 0, 4, 0, 2, 6, 0], 
-        [0, 0, 1, 0, 5, 0, 9, 3, 0], 
-        [9, 0, 4, 0, 6, 0, 0, 0, 5], 
-        [0, 7, 0, 3, 0, 0, 0, 1, 2], 
-        [1, 2, 0, 0, 0, 7, 4, 0, 0], 
-        [0, 4, 9, 2, 0, 6, 0, 0, 7] 
-
-]
-
->>>>>>> 0f2162b214bc79afcf3bb49e7aa04a5f7852a048
